8ed5c26e9300486bd714484ed8a07f6c72194e06
iElya <mdv.elya@gmail.com>
2022-12-19 19:35:57 +0400

    massive

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+let games = [
+    ['Resident Evil' , 'outlast' , 'The Evil Within' , 'Dying light' , 'Silent Hill' ] ,
+    ['Battlefield' , 'Call of duty' , 'Killzone' , 'resistance' , 'Pubg'],
+    ['Batman' , 'The last Of Us' , 'Devil May cry' ]
+]
+document.write(games[0][0][9]+games[2][1][4]+games[0][3][1]+games[1][1][1])
+console.log(games[0][0][9]+games[1][1][2]+ games[2][0][3]+games[1][3][3]+games[1][3][0]+games[2][1][5]+ " "+ games[2][2][6]+games[0][1][4]+games[2][0][3]+ games[2][0][3]+games[1][3][6]+games[1][1][8]+games[0][1][0]+games[0][2][5]+games[1][0][1])
